45add14d55074e82ce4f2f25232117cef70d66c7
James <jammatpic@gmail.com>
2016-02-15 03:04:24 +1100

    Added basic interaction with API

--- resources/js/wikipedia-viewer.js
+++ resources/js/wikipedia-viewer.js
@@ -1,10 +1,20 @@
+
 $(document).ready(function() {
     $("#search-form").submit(function(event) {
         event.preventDefault()
         var input = $(".search-bar").val();
+        var html = "";
         var apiCall;
-        /*$.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert+Einstein&srprop=snippet&srlimit=10", function(json) {
-            alert(json.search[0].title);
-        });*/
+        $.ajax({
+            url: "http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert+Einstein&srprop=snippet&srlimit=10",
+            dataType: "jsonp",
+            success: function(json) {
+                for (var key in json.query.search[0]) {
+                    html += json.query.search[0][key];
+                }
+                alert(html);
+            }
+        });
+
     });
 });
