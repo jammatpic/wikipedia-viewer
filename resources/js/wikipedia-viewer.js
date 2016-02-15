318fa1834e889c8409a7ffab32eb74124fe00f88
James <jammatpic@gmail.com>
2016-02-15 17:48:59 +1100

    Minor changes to layout, more comments in javascript, handling edge cases

--- resources/js/wikipedia-viewer.js
+++ resources/js/wikipedia-viewer.js
@@ -1,24 +1,39 @@
 $(document).ready(function() {
     $("#search-form").submit(function(event) {
         event.preventDefault()
-        var input = $(".search-bar").val();
-        input = input.replace(/d\\+/g, "+");
-        var html = "";
-        var apiCall;
-        $.ajax({
-            url: "http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srprop=snippet&srlimit=10&srsearch="+input,
-            dataType: "jsonp",
-            success: function(json) {
-                for (var key in json.query.search) {
-                    // building link
-                    title = json.query.search[key].title;
-                    title = title.replace(/ /g, "_");
-                    // displaying title and description for each returned page
-                    $("<h4 class='bold title'>"+"<a href='https://en.wikipedia.org/wiki/"+title+"'>"+json.query.search[key].title+"</a></h4>").appendTo(".results");
-                    $("<p class='description'>"+json.query.search[key].snippet+"...</p>").appendTo(".results");
-                }
+        // all in function after in order to sequence fade animations
+        $('.results').fadeOut(300, function() { 
+            $(this).empty();
+            var input = $(".search-bar").val();
+            input = input.replace(/d\\+/g, "+");
+            var html = "";
+            var apiCall;
+            if (input == "") {
+                $("<h4 class='bold title text-center'>No results found</h4>").appendTo(".results");
+                $('.results').fadeIn(300);
+            } else {
+                $.ajax({
+                //adding input to api call
+                    url: "http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srprop=snippet&srlimit=10&srsearch="+input,
+                    dataType: "jsonp",
+                    success: function(json) {
+                        if (json.query.searchinfo.totalhits === 0) {
+                            $("<h4 class='bold title text-center'>No results found</h4>").appendTo(".results");
+                            $('.results').fadeIn(300);
+                        } else {
+                            for (var key in json.query.search) {
+                                // building link
+                                title = json.query.search[key].title;
+                                title = title.replace(/ /g, "_");
+                                // displaying title and description for each returned page
+                                $("<h4 class='bold title'>"+"<a href='https://en.wikipedia.org/wiki/"+title+"'>"+json.query.search[key].title+"</a></h4>").appendTo(".results");
+                                $("<p class='description'>"+json.query.search[key].snippet+"...</p>").appendTo(".results");
+                            }
+                            $('.results').fadeIn(300);
+                        }
+                    }
+                });
             }
         });
-
     });
 });
